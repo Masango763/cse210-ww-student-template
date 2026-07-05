03e46d3120f9939d69daa854e58250170735d38a
Masango763 <dewhemasango@gmail.com>
2026-07-05 15:57:00 +0200

    updating week02

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _prompt;
+    public string _text;
+
+    public void Display()
+    {
+        Console.WriteLine("------------------------------");
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_prompt}");
+        Console.WriteLine($"Entry: {_text}");
+        Console.WriteLine("------------------------------");
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator generator = new PromptGenerator();
+
+        string choice = "";
+
+        while (choice != "5")
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Exit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Entry entry = new Entry();
+
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._prompt = generator.GetRandomPrompt();
+
+                Console.WriteLine(entry._prompt);
+                entry._text = Console.ReadLine();
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+        }
     }
 }
