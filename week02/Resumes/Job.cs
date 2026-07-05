06f86cf1cb51a3f352e8762694b7b1e0c4daa969
Masango763 <dewhemasango@gmail.com>
2026-07-05 15:26:28 +0200

    Updated Job, Resume, and Program for Week 02 assignment

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -2,18 +2,11 @@ using System;
 
 public class Job
 {
-    // Attributes (Member Variables)
     public string _jobTitle = "";
     public string _company = "";
     public int _startYear;
     public int _endYear;
 
-    // Constructor
-    public Job()
-    {
-    }
-
-    // Method to display job details
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,29 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // 1. Create instance for the first job
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
+        job1._company = "Google";
+        job1._startYear = 2020;
         job1._endYear = 2022;
 
-        // 2. Create instance for the second job
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Microsoft";
         job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._endYear = 2025;
 
-        // 3. Create the main Resume container instance
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Masango";
 
-        // 4. Add both distinct job instances into the resume's list
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        // 5. Run the Display method to cleanly unpack and print everything
         myResume.Display();
     }
 }
