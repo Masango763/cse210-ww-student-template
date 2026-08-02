f9258170119a2cf086b882888bcb20c1fb03095d
Masango763 <dewhemasango@gmail.com>
2026-08-03 00:02:33 +0200

    Add IPrintable interface and update shapes project files

---
+++ week06/Shapes/IPrintable.cs
@@ -0,0 +1,6 @@
+// Interface defining a strict contract that any printable object must implement.
+public interface IPrintable
+{
+    // Method signature requiring classes to provide detail display logic.
+    void DisplayDetails();
+}
